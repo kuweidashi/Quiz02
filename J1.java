8149c90a19e1020c4227f846866b0012946764d0
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-12-21 11:08:56 +0800

    Update J1.java

--- J1.java
+++ J1.java
@@ -3,32 +3,29 @@ import java.util.Scanner;
 
 public class J1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("密碼輸入測試");
-        System.out.print("請輸入密碼:");
-        int a = sc.nextInt();
-        for(int i=0;i<3;i++)
-        {      
-            System.out.print("請再輸入一次密碼:");
-            int b = sc.nextInt();
-            if (b==a)
-            {
-                System.out.println("密碼正確");
-                break;
-            } else {
-                System.out.println("與第一次輸入的不同");
-                
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("密碼輸入測試");
+            System.out.print("請輸入密碼:");
+            int a = sc.nextInt();
+            for(int i=0;i<3;i++)
+            {      
+                System.out.print("請再輸入一次密碼:");
+                int b = sc.nextInt();
+                if (b==a)
+                {
+                    System.out.println("密碼正確");
+                    break;
+                } else {
+                    System.out.println("與第一次輸入的不同");
+                    
+                }
+                if (i==2)
+                {
+                    throw new RuntimeException("輸入三次錯誤！程式停止！");
+                }    
             }
-            if (i==2)
-            {
-                throw new RuntimeException("輸入三次錯誤！程式停止！");
-            }    
-        }
-        try {  
-        } catch (RuntimeException e) {
-            System.out.println(e);
-        }
-        sc.close();      
+            sc.close();
+        }      
     }
 }
 
