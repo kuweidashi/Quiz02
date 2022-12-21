1cdfabc52444d5178259ba09626948a39c0e1c0e
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-12-21 11:06:04 +0800

    Update J1.java

--- J1.java
+++ J1.java
@@ -1,30 +1,34 @@
 package Quiz02;
-
 import java.util.Scanner;
-import java.io.IOException;
 
 public class J1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("密碼輸入測試");
         System.out.print("請輸入密碼:");
         int a = sc.nextInt();
         for(int i=0;i<3;i++)
-        {
-            try {
-                System.out.print("請再輸入一次密碼:");
-                int b = sc.nextInt();
-                if (b==a)
-                {
-                    System.out.println("密碼正確");
-                    break;
-                } else {
-                    throw new RuntimeException();
-                }
-            } catch (RuntimeException e) {
+        {      
+            System.out.print("請再輸入一次密碼:");
+            int b = sc.nextInt();
+            if (b==a)
+            {
+                System.out.println("密碼正確");
+                break;
+            } else {
                 System.out.println("與第一次輸入的不同");
+                
             }
+            if (i==2)
+            {
+                throw new RuntimeException("輸入三次錯誤！程式停止！");
+            }    
+        }
+        try {  
+        } catch (RuntimeException e) {
+            System.out.println(e);
         }
         sc.close();      
     }
 }
+
